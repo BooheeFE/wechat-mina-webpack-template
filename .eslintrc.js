46dd814516f9636a8d09812be91efafe7f3604a3
simbawu <code@simbawu.com>
2018-08-28 14:14:26 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: "booheefe",
+  // add your custom rules here
+  "rules": {
+    // allow debugger during development
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+  },
+  globals: {
+    App: true,
+    Page: true,
+    wx: true,
+    getApp: true,
+    Component: true,
+    getCurrentPages: true
+  }
+};
