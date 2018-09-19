1b5815f3bf2e1171c0a09673f2bd9164b2e76d90
simbawu <code@simbawu.com>
2018-09-19 15:54:05 +0800

    chore: use clean-webpack-plugin

--- config/webpack/webpack.prod.config.js
+++ config/webpack/webpack.prod.config.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -15,6 +17,11 @@ module.exports = {
     ]
   },
   plugins: [
-    new BundleAnalyzerPlugin()
+    new BundleAnalyzerPlugin(),
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname, '../../'),
+      verbose: true,
+      exclude: ['project.config.json']
+    })
   ]
 };
